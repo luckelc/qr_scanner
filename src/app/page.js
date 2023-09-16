c2ff53120ea65a7b70f6d16ac84290624008989e
Lucas <lucaschristiansson05@gmail.com>
2023-09-16 23:14:23 +0200

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,51 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import React, { useEffect, useState } from 'react';
 
-export default function Home() {
+import { Html5QrcodeScanner } from 'html5-qrcode';
+
+import styles from './page.module.css';
+
+import questions from './questions.json'
+
+function App() {
+
+  console.log(questions.questions);
+
+  var questionList = []
+
+  questions.questions.forEach((e) => {
+    questionList.push(
+    <li key={e.id} className={styles.question}>
+      <div style={{backgroundColor: e.color.hex}}>
+        <h3>{e.question}</h3>
+      </div>
+    </li>)
+  })
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    <div className={styles.app}>
+
+      <h1 className={styles.h1}>QR Scanning Code</h1>
+
+      <ul>
+        {questionList}
+      </ul>
+
+      <div className={styles.nav}>
+        <div className={styles.navGroup}>
+          <div className={styles.scan}>SCAN</div>
+          <div className={styles.give_up}>GIVE UP</div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+  );
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
 }
+
+
+
+export default App;   
