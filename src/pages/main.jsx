2c8e9ef9df957a273fd1b701461c112692dc6fff
lucas christiansson <lucaschristiansson05@protonmail.com>
2023-10-26 16:48:08 +0300

    removed favicon

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -156,7 +156,6 @@ export default function QrScannerHomePage() {
 					name="viewport"
 					content="width=device-width, initial-scale=1"
 				/>
-				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<div className={styles.main}>
