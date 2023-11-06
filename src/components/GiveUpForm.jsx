989b4e80cbaa49b25baaf31afafd706dcf5c4404
lucas christiansson <98053131+luckelc@users.noreply.github.com>
2023-11-06 22:51:13 +0100

    Switched to swedish

--- src/components/GiveUpForm.jsx
+++ src/components/GiveUpForm.jsx
@@ -5,16 +5,16 @@ export default function GiveUpForm({ canceled, proceeded }) {
 	return (
 		<div className={styles.giveUpForm}>
 			<div className={styles.card}>
-				<h2>Would you like to give up?</h2>
+				<h2>Vill du ge upp?</h2>
 				<ul>
 					<li>
 						<button type="button" onClick={() => canceled()}>
-							Cancel
+							Nej
 						</button>
 					</li>
 					<li>
 						<button type="button" onClick={() => proceeded()}>
-							Proceed
+							Ja
 						</button>
 					</li>
 				</ul>
